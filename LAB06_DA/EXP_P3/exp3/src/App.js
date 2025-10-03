f9c0de67856f9285f2c48659aa481f12de528d67
MauricioLazo22 <mauricio.lazo@estudiante.ucsm.edu.pe>
2025-10-03 08:14:51 -0500

    exp3

--- LAB06_DA/EXP_P3/exp3/src/App.js
+++ LAB06_DA/EXP_P3/exp3/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Table from './Table';
+import './styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    const characters = [
+      { name: 'Mauricio Lazo', job: 'Mantenido' },
+      { name: 'Ivancito Gamer', job: 'Mantenido ' },
+      { name: 'Sebastian Castro', job: 'Mantenido' },
+    ];
+
+    return (
+      <div>
+        <h1>Tablita prueba</h1>
+        <Table characterData={characters} />
+      </div>
+    );
+  }
 }
 
 export default App;
