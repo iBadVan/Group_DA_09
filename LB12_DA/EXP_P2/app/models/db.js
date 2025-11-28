cf42dff152ea762c8217dbc935c05b3dab884782
MauricioLazo22 <mauricio.lazo@estudiante.ucsm.edu.pe>
2025-11-28 00:24:00 -0500

    EXP_P2 FIX END

--- LB12_DA/EXP_P2/app/models/db.js
+++ LB12_DA/EXP_P2/app/models/db.js
@@ -2,6 +2,6 @@
 var mongoose = require('mongoose');
 // Estableciendo conexion con MongoDB
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/node-crud',{
-    useNewUrlParser : true
-});
+mongoose.connect('mongodb://127.0.0.1:27017/node-crud');
+
+module.exports = mongoose;
