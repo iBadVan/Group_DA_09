9007323fd506ef57af22d337076dddc48fb06cfd
MauricioLazo22 <mauricio.lazo@estudiante.ucsm.edu.pe>
2025-11-28 00:54:49 -0500

    END EXP_P2

--- LB12_DA/EXP_P2/app/controllers/routes.js
+++ LB12_DA/EXP_P2/app/controllers/routes.js
@@ -1,34 +1,46 @@
-// Importando paquetes y archivos necesarios para trabajar las rutas
 var express = require('express');
 var bodyParser = require('body-parser');
 var Product = require('../models/products');
+
 var router = express.Router();
-// Configurando el Parser
-router.use(bodyParser.urlencoded({extended:true}));
+
+// Configurar parser
+router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
-router.use(function(req,res,next){
-    console.log("request");
-    next();
+
+// Middleware de prueba
+router.use(function(req, res, next) {
+  console.log("request");
+  next();
 });
 
-// Metodos para manejar Base de Datos mongoDB llamada node-crud
+// POST y GET
 router.route('/products')
-.post(function(req,res){
-var product = new Product();
-product.name = req.body.name;
-product.amount = req.body.amount;
-product.description = req.body.description;
-product.save(function(error){
-    if (error)
-        res.status(500).send("Error en el servicio"+error);
-        res.json({message:"Producto registrado con exito"});
+
+.post(async function(req, res) {
+  try {
+    var product = new Product({
+      name: req.body.name,
+      amount: req.body.amount,
+      description: req.body.description
     });
-    })
-    .get(function(req,res){
-    Product.find(function(error,products){
-    if (error)
-        res.status(500).send("Error en el servicio"+error);
+
+    await product.save();
+    res.json({ message: "Producto registrado con éxito" });
+
+  } catch (error) {
+    res.status(500).send("Error en el servicio: " + error);
+  }
+})
+
+.get(async function(req, res) {
+  try {
+    const products = await Product.find();
     res.json(products);
-    });
+  } catch (error) {
+    res.status(500).send("Error en el servicio: " + error);
+  }
 });
+
+
 module.exports = router;
